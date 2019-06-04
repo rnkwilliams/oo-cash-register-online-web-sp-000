9e7a0a6f94386acf4246fe1b2760b2422fd82604
Ranika Williams <rnkprice07@gmail.com>
2019-06-04 02:44:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,6 +22,16 @@ class CashRegister
   end
 
   def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
   end
 
+  def void_last_transaction
+    @total -= @price
   end
+end
