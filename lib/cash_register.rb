9a366c5fe4702dbfa96cc7389fcc2b1ebbaf1a11
Ranika Williams <rnkprice07@gmail.com>
2019-06-04 02:33:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,27 @@
 class CashRegister
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, :price, :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-
+    @items = []
   end
 
   def add_item(item, price, quantity = 1)
-    
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
+
+
   end
 
 
